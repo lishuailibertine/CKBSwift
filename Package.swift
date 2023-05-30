47ce3f2ae8a62c2cba3b317942dbac9b0f63b481
lishuailibertine <libertine9458@gmail.com>
2023-05-30 11:54:05 +0800

    update Secp256k1Swift 2.0.0

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(name: "Secp256k1Swift", url: "https://github.com/mathwallet/Secp256k1Swift.git", from: "1.3.1"),
+        .package(name: "Secp256k1Swift", url: "https://github.com/mathwallet/Secp256k1Swift.git", from: "2.0.0"),
         .package(name: "Blake2", url: "https://github.com/lishuailibertine/Blake2.swift", from: "0.1.3"),
         .package(url: "https://github.com/krzyzanowskim/CryptoSwift", from: "1.4.2"),
         .package(url: "https://github.com/attaswift/BigInt", from: "5.3.0"),
